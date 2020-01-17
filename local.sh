998e933077a301e3dc547646b94be72b33833d51
breakwa2333 <48397771+breakwa2333@users.noreply.github.com>
2020-01-17 14:58:43 +0800

    Update local.sh

--- local.sh
+++ local.sh
@@ -8,12 +8,12 @@ main(){
   touch ./demoCA/index.txt
   touch ./demoCA/index.txt.attr
   touch ./demoCA/serial
-  echo 01 > ./demoCA/serial
+  echo 02 > ./demoCA/serial
   wget -O ./demoCA/conf/ca.conf https://raw.githubusercontent.com/breakwa2333/FakeCA/master/ca.conf
-  wget -O ./server/conf/server.conf https://raw.githubusercontent.com/breakwa2333/FakeCA/master/server.conf
-  local_ip=`curl -4 ip.sb`
+  wget -O ./server/conf/localhost.conf https://raw.githubusercontent.com/breakwa2333/FakeCA/master/server.conf
+  local_ip=127.0.0.1
   echo "IP.1 = $local_ip" >> ./server/conf/server.conf
   openssl ecparam -genkey -name prime256v1 -out ./demoCA/private/cakey.pem
-  openssl ecparam -genkey -name prime256v1 -out ./server/private/server.key
+  openssl ecparam -genkey -name prime256v1 -out ./server/private/localhost.key
 }
 main
