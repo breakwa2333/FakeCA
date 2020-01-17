7c8d24de84434d22dbaf2f9ae2bf1ad436b9835d
breakwa2333 <48397771+breakwa2333@users.noreply.github.com>
2020-01-17 14:56:44 +0800

    Add files via upload

---
+++ local.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+main(){
+  apt-get update
+  apt-get install openssl
+  mkdir -p ./demoCA/{private,newcerts,conf}
+  mkdir -p ./server/{private,request,conf}
+  touch ./demoCA/index.txt
+  touch ./demoCA/index.txt.attr
+  touch ./demoCA/serial
+  echo 01 > ./demoCA/serial
+  wget -O ./demoCA/conf/ca.conf https://raw.githubusercontent.com/breakwa2333/FakeCA/master/ca.conf
+  wget -O ./server/conf/server.conf https://raw.githubusercontent.com/breakwa2333/FakeCA/master/server.conf
+  local_ip=`curl -4 ip.sb`
+  echo "IP.1 = $local_ip" >> ./server/conf/server.conf
+  openssl ecparam -genkey -name prime256v1 -out ./demoCA/private/cakey.pem
+  openssl ecparam -genkey -name prime256v1 -out ./server/private/server.key
+}
+main
